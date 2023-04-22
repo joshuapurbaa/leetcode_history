9e42dd358d34c0a0f618bc226447558ba1d71307
joshuapurbaa <yoshuapurba06@gmail.com>
2023-04-22 08:58:36 +0700

    initial commit

---
+++ bin/leetcode_history.dart
@@ -0,0 +1,5 @@
+import 'package:leetcode_history/leetcode_history.dart' as leetcode_history;
+
+void main(List<String> arguments) {
+  print('Hello world: ${leetcode_history.calculate()}!');
+}
---
+++ test/leetcode_history_test.dart
@@ -0,0 +1,8 @@
+import 'package:leetcode_history/leetcode_history.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
